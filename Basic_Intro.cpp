90973b152494dc2f2eb3d855400af4b3c2f52452
Rajyavardhan Singh Bais <rvsbais21@gmail.com>
2026-07-05 07:28:51 +0530

    Basic Introduction Uploaded.

---
+++ Basic_Intro.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, my name is Rajyavardhan Singh Bais." << endl << "I am 18 years old." << endl << "I am from Ghaziabad, Uttar Pradesh, India." << endl;
+    return 0;
+}
