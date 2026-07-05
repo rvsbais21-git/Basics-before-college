0cdbcc2b572c18fe88fe09ebe8bf899bf5b1f5a8
Rajyavardhan Singh Bais <rvsbais21@gmail.com>
2026-07-05 08:02:17 +0530

    Area of rectangle practiced.

---
+++ Area-rec.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b;
+    cout << "Enter number a (in cm): ";
+    cin >> a;
+    cout << "Enter number b: (in cm)";
+    cin >> b;
+    cout << "Area of the Rectangle: " << a*b << "cm^2";
+}
