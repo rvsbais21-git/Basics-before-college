fd166d39b84c40d3b0646e23dda36b74e7cc1a2c
Rajyavardhan Singh Bais <rvsbais21@gmail.com>
2026-07-05 13:02:36 +0530

    Reverse Series

---
+++ reve.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int i,a;
+    cout <<"Enter number: ";
+    cin>>a;
+    for(i=a;i>0;i--) {
+        cout<< i << " ";
+    }
+    return 0;
+}
