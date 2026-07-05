ed5b8963092c18b452bdf11f50e6af8bdb89743a
Rajyavardhan Singh Bais <rvsbais21@gmail.com>
2026-07-05 13:00:00 +0530

    Series

---
+++ series.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int i,a;
+    cout <<"Enter number: ";
+    cin>>a;
+    for(i=1;i<=a;i++) {
+        cout<< i << " ";
+    }
+    return 0;
+}
