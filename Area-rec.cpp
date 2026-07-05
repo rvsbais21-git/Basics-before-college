3edfa52c0e8c99af4044f012071206279e7f428d
Rajyavardhan Singh Bais <rvsbais21@gmail.com>
2026-07-05 09:22:29 +0530

    Minor update.

--- Area-rec.cpp
+++ Area-rec.cpp
@@ -3,6 +3,7 @@ using namespace std;
 int main() {
     float a, b;
     cout << "Enter number a (in cm): ";
+    cin>> a;
     cout << "Enter number b: (in cm)";
     cin >> b;
     cout << "Area of the Rectangle: " << a*b << "cm^2";
